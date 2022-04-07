be0f07f3536ece428d05b1f24caf33fa5e82e4ca
Akitetsuk <m.feijoo.1995@gmail.com>
2022-04-07 09:31:16 +0200

    VUE-counter-project

--- js/app.js
+++ js/app.js
@@ -1,2 +1,40 @@
 // Tu código aquí.
 
+Vue.createApp({
+    data() {
+      return {
+        counter: 0
+      }
+    },
+  
+    methods:{
+        buttonPrev(){
+         // TODO: Decrementar el index
+         this.counter--
+        
+      },
+      buttonNext(){
+         // TODO: Incrementar el index
+         this.counter++
+       
+      }
+    
+    },
+  
+    computed: {
+      getColor() {
+          if(this.counter<0){
+              return 'red'
+          }
+          else if(this.counter>0){
+              return'green'
+          }
+          else{
+              return 'grey'
+          }
+        // return `url('./img/${pictures[this.index]}.jpeg')`; // tenemos que devolver un string a la ruta de la foto actual. Por ejemplo, la ruta de la primera foto es '../img/pictures[0]'
+      }
+    }
+  
+  }).mount('#app')
+
